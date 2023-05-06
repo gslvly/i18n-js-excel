94acac730043e992673cf5c5c9332b8495df7602
gs <605661239@qq.com>
2023-05-06 10:32:16 +0800

    save

--- pub.sh
+++ pub.sh
@@ -18,3 +18,18 @@
 # done
 # echo 成功！
 
+ 
+
+echo '1、代码上传'
+
+message="cd /root/webroot/i18n-js-excel && rm -rf *"
+
+echo '2、执行远程命令：' "$message"
+ssh root@web-play.cn "$message"
+
+echo '3、传送'
+scp -r ./* root@web-play.cn:/root/webroot/i18n-js-excel
+
+echo '代码发布完成'
+
+
